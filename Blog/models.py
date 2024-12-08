029dbd5b700bcc15798ae220aeeaa6303efba3b0
YASIR AHMAD <yazirahmed123@gmail.com>
2024-12-08 13:03:27 +0300

    installed pillow and added banner Field to the model

--- Blog/models.py
+++ Blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='placeholder.jpg' , blank=True)
 
     def __str__(self):
         return self.title
